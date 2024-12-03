f39e0600a8007fbbe080506165057dd4073a5a0e
Mitsuki <Mitsukikanna@outlook.com>
2024-12-04 00:30:06 +0800

    part4 end

---
+++ src/sprite.cpp
@@ -0,0 +1,36 @@
+#include <SDL_render.h>
+#include <cstdio>
+#include <sprite.h>
+#include <graphics.h>
+#include <string>
+#include <global.h>
+
+Sprite::Sprite(){}
+
+Sprite::Sprite(Graphics &graphics, const std::string &filePath, int sourceX, int sourceY, int width, int height, float posX, float posY):_X(posX),_y(posY)
+{
+    this->_sourceRect.x = sourceX;
+    this->_sourceRect.y = sourceY;
+    this->_sourceRect.w = width;
+    this->_sourceRect.h = height;
+
+    this->_spriteSheet =SDL_CreateTextureFromSurface(graphics.getRenderer(), graphics.loadImage(filePath));
+
+    if(!this->_spriteSheet)
+    {
+        printf("\nError: Unable to load image\n");
+    }
+}
+
+Sprite::~Sprite() {}
+
+void Sprite::draw(Graphics &graphics, int x, int y)
+{
+    SDL_Rect destinationRectangle = { x,y,this->_sourceRect.w * static_cast<int>(globals::SPRITE_SCALE), this->_sourceRect.h * static_cast<int>(globals::SPRITE_SCALE)};
+    graphics.blitSurface(this->_spriteSheet, &this->_sourceRect, &destinationRectangle);
+}
+
+void Sprite::update()
+{
+
+}
